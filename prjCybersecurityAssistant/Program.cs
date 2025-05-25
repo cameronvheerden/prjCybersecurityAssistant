47e9c1bc77e21515c8b271743d2183c8c801fc84
Cameron van Heerden <ST10435031@vcconnect.edu.za>
2025-05-26 00:21:11 +0200

    Restarting whole code and enhancing my code by optimising everything

--- prjCybersecurityAssistant/Program.cs
+++ prjCybersecurityAssistant/Program.cs
@@ -7,23 +7,6 @@
             // Create an instance of the CyberSecurity class
             CyberSecurity cyber = new CyberSecurity();
 
-            // Start the chatbot functionality
-            cyber.StartChatbot();
-            //
         }
     }
 }
-/*
- Reference List
-    https://patorjk.com/software/taag/#p=testall&f=Graffiti&t=
-    Chand, M., 2023. How to Generate a Random Number and Random String in C#?. [Online]
-   Available at: https://www.c-sharpcorner.com/article/generating-random-number-and-string-in-C-Sharp/
-   [Accessed 20 April 2025].
-   George, A. (. D., 2023. How to: Play a Sound from a Windows Form. [Online]
-   Available at: https://learn.microsoft.com/en-us/dotnet/desktop/winforms/controls/how-to-play-a-sound-from-a-windows-form?view=netframeworkdesktop-4.8
-   [Accessed 20 April 2025].
-   Motta, J., 2023. Using System.Speech with .NET 7. [Online]
-   Available at: https://www.c-sharpcorner.com/article/using-system-speech-with-net-7/#:~:text=In%20this%20article%2C%20I%27ll%20demonstrate%20how%20to%20use,recognition%20and%20text-to-speech%20and%20provides%20a%20unified%20API.
-   [Accessed 20 April 2025].
-   Troelsen, A. & Japikse, P., 2021. Pro C# 9 with .NET 5: Foundational Principles and Practices in Programming. 10th ed. New York: Apress Berkeley, CA.
-*/
