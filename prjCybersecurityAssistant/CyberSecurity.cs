54316e13a05cc0f099b77ba6aeaa41754731a12a
Cameron van Heerden <ST10435031@vcconnect.edu.za>
2025-05-26 21:55:44 +0200

    Push reference list

--- prjCybersecurityAssistant/CyberSecurity.cs
+++ prjCybersecurityAssistant/CyberSecurity.cs
@@ -414,3 +414,27 @@ namespace prjCybersecurityAssistant
         }
     }
 }
+/*
+Reference List
+Corey, T., 2023. The Dictionary Data Structure in C# in 10 Minutes or Less. [Online] 
+Available at: https://www.youtube.com/watch?v=R94JHIXdTk0&list=TLPQMjUwNTIwMjXgYKPv1HBduQ&index=4
+[Accessed 26 May 2025].
+Manson, J., 2014. C# Development Tutorial | Speech Synthesis. [Online] 
+Available at: https://www.youtube.com/watch?v=gkTiKMSLOHY&list=TLPQMjUwNTIwMjV5DPeJRoATSg&index=5
+[Accessed 25 May 2025].
+Monkey, C., 2020. What are Delegates? (C# Basics, Lambda, Action, Func). [Online] 
+Available at: https://www.youtube.com/watch?v=3ZfwqWl-YI0&list=TLPQMjUwNTIwMjXgYKPv1HBduQ&index=2
+[Accessed 26 May 2025].
+O'Didily, M., 2022. How to Play Music In C#. [Online] 
+Available at: https://youtu.be/uZlz1SSisYY?si=aH0bDU7yFD15kiSU
+[Accessed 25 May 2025].
+Opus), B. (., 2013. C# Text to Speech using Speech.Synthesis. [Online] 
+Available at: https://www.youtube.com/watch?v=4zdmSVgtfkU&list=TLPQMjUwNTIwMjXgYKPv1HBduQ&index=1
+[Accessed 26 May 2025].
+Plays, K., 2022. C# Tutorial for Beginners #33 - Random. [Online] 
+Available at: https://www.youtube.com/watch?v=Ww5897-l_K0
+[Accessed 26 May 2025].
+Programming, J. T., 2019. C# List Tutorial How to Use Lists. [Online] 
+Available at: https://www.youtube.com/watch?v=LJAxwrIw0SA&list=TLPQMjUwNTIwMjXgYKPv1HBduQ&index=16
+[Accessed 26 May 2025].
+*/
